328e8dec473551b75d462a2317d19c191170d351
Ghostdragoon <A.khan@student.saedubai.com>
2023-07-14 02:45:31 +0400

    Level 1 with test Ai
    Ai half done, can run random cover, shoot, duck and reload all with animations. level mapped out and residecial area buildings assigned, market buildings assigned and fortress assigned

--- GAD181/Assets/Scenes/EnemyAi.cs
+++ GAD181/Assets/Scenes/EnemyAi.cs
@@ -1,47 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+
 
 public class EnemyAi : MonoBehaviour
 {
     public Transform target;
 
-
+    private EnemyAiRef enemyReferences;
 
     private float shootingDistance;
 
-    private float pathUpdateDeadline;
-    public NavMeshAgent navMeshagent;
-    public Animator animator;
-
+    private float pathUpdateDeadline;   
 
     private void Awake()
     {
-        navMeshagent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
+        enemyReferences = GetComponent<EnemyAiRef>();
     }
 
-    void Start ()
+    // Start is called before the first frame update
+    void Start()
     {
-        shootingDistance = navMeshagent.stoppingDistance;
+        shootingDistance = enemyReferences.navMeshAgent.stoppingDistance;
     }
 
-    void Update ()
+    // Update is called once per frame
+    void Update()
     {
         if (target != null)
         {
             bool inRange = Vector3.Distance(transform.position, target.position) <= shootingDistance;
 
-            if (inRange)
-            {
+            if (inRange) 
+                {
+
                 LookAtTarget();
-            }
+
+                } 
             else
             {
                 UpdatePath();
             }
+            enemyReferences.animator.SetBool("Shooting", inRange);
         }
+        enemyReferences.animator.SetFloat("Speed", enemyReferences.navMeshAgent.desiredVelocity.sqrMagnitude);
     }
 
     private void LookAtTarget()
@@ -51,9 +53,14 @@ public class EnemyAi : MonoBehaviour
         Quaternion rotation = Quaternion.LookRotation(lookPos);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.2f);
     }
+
     private void UpdatePath()
     {
-           navMeshagent.SetDestination(target.position);
+        if (Time.time >= pathUpdateDeadline)
+        {
+            Debug.Log("Updating Path ...");
+            pathUpdateDeadline = Time.time + enemyReferences.pathUpdateDelay;
+            enemyReferences.navMeshAgent.SetDestination(target.position);
+        }
     }
-
 }
---
+++ GAD181/Assets/Scenes/IState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public interface IState
+    {
+        void Tick();
+        void OnEnter();
+        void OnExit();
+        Color GizmoColor();
+    }
